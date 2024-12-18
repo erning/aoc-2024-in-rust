0aaf001a16ef0d654e39d7ad71b56d35bbf03a06
Zhang Erning <zhang@erning.com>
2024-12-18 10:00:09 +0800

    refactor

--- src/day17.rs
+++ src/day17.rs
@@ -4,8 +4,6 @@ struct Computer {
     b: u64,
     c: u64,
     p: usize,
-    program: Vec<u8>,
-    output: Vec<u8>,
 }
 
 impl Computer {
@@ -19,57 +17,51 @@ impl Computer {
         }
     }
 
-    fn exec_once(&mut self) -> bool {
-        let opcode = self.program[self.p];
-        let operand = self.program[self.p + 1];
+    fn exec_once(&mut self, program: &[u8], output: &mut Vec<u8>) -> bool {
+        let opcode = program[self.p];
+        let operand = program[self.p + 1];
         self.p += 2;
         match opcode {
-            0 => self.a >>= self.combo(operand),
-            1 => self.b ^= operand as u64,
-            2 => self.b = self.combo(operand) % 8,
-            3 if self.a != 0 => self.p = operand as usize,
-            4 => self.b ^= self.c,
-            5 => self.output.push(self.combo(operand) as u8 % 8),
-            6 => self.b = self.a >> self.combo(operand),
-            7 => self.c = self.a >> self.combo(operand),
+            /* adv */ 0 => self.a >>= self.combo(operand),
+            /* bxl */ 1 => self.b ^= operand as u64,
+            /* bst */ 2 => self.b = self.combo(operand) % 8,
+            /* jnz */ 3 if self.a != 0 => self.p = operand as usize,
+            /* bxc */ 4 => self.b ^= self.c,
+            /* out */ 5 => output.push(self.combo(operand) as u8 % 8),
+            /* bdv */ 6 => self.b = self.a >> self.combo(operand),
+            /* cdv */ 7 => self.c = self.a >> self.combo(operand),
             _ => {}
         }
-        self.p < self.program.len()
+        self.p < program.len()
     }
 
-    fn execute(&mut self) {
-        while self.exec_once() {}
+    fn execute(&mut self, program: &[u8]) -> Vec<u8> {
+        self.p = 0;
+        let mut output = Vec::new();
+        while self.exec_once(program, &mut output) {}
+        output
     }
 }
 
-fn parse_input(input: &str) -> Computer {
+fn parse_input(input: &str) -> (Computer, Vec<u8>) {
     const REGI_PREFIX_LEN: usize = "Register #:".len();
     const PROG_PREFIX_LEN: usize = "Program:".len();
     let lines: Vec<&str> = input.trim().lines().collect();
     let a = lines[0][REGI_PREFIX_LEN..].trim().parse().unwrap();
     let b = lines[1][REGI_PREFIX_LEN..].trim().parse().unwrap();
     let c = lines[2][REGI_PREFIX_LEN..].trim().parse().unwrap();
-    let p = 0;
     let program = lines[4][PROG_PREFIX_LEN..]
         .trim()
         .split(',')
         .map(|s| s.trim().parse().unwrap())
         .collect();
 
-    Computer {
-        a,
-        b,
-        c,
-        p,
-        program,
-        output: Vec::new(),
-    }
+    (Computer { a, b, c, p: 0 }, program)
 }
 
 pub fn part_one(input: &str) -> String {
-    let mut comp = parse_input(input);
-    comp.execute();
-    comp.output
+    let (mut comp, program) = parse_input(input);
+    comp.execute(&program)
         .iter()
         .map(|v| v.to_string())
         .collect::<Vec<String>>()
@@ -77,16 +69,13 @@ pub fn part_one(input: &str) -> String {
 }
 
 pub fn part_two(input: &str) -> u64 {
-    let mut comp = parse_input(input);
+    let (mut comp, program) = parse_input(input);
     let mut a = 1;
-    let proglen = comp.program.len();
     loop {
         comp.a = a;
-        comp.p = 0;
-        comp.output.clear();
-        comp.execute();
-        if comp.output == comp.program[proglen - comp.output.len()..] {
-            if comp.output.len() == proglen {
+        let output = comp.execute(&program);
+        if output == program[program.len() - output.len()..] {
+            if output.len() == program.len() {
                 break;
             }
             a <<= 3;
